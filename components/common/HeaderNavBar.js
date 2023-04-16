5065dc52f7bae4cf5beb1e7bb0a6e6fc8ec73316
Pedro <pedrosarmet97@gmail.com>
2023-04-16 01:16:45 -0300

    primeiro commit

---
+++ components/common/HeaderNavBar.js
@@ -0,0 +1,11 @@
+import styles from "../styles/index.module.css";
+
+export default function HeaderNavBar() {
+    return (
+        <>
+            <div className={styles.headerNavBar}>
+                <p className={styles.textNavBar}>hello</p>
+            </div>
+        </>
+    )
+}
